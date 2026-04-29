c79c77c1a16ead65e28140dcd4713171361245d1
samuel won <samuelwon5528@gmail.com>
2026-04-29 11:07:48 +0800

    bmi.py

--- bmi.py
+++ bmi.py
@@ -3,5 +3,11 @@ def calculate_bmi(height, weight):
  print("Weight = " + str(weight))
  bmi = (weight/(height * height))
  print("BMI = " + str(bmi))
+ if (bmi <= 18):
+  print("Underweight")
+ elif (bmi >= 25):
+  print("Overweight")
+ else:
+  print("Healthy Weight")
 calculate_bmi(height = 1.65, weight= 52)
 
